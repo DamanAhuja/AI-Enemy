f219bc96b1a4bcdc286068b1c7ca4ab76ecf7a67
Daman Ahuja <damanahuja09@gmail.com>
2025-03-16 16:04:09 +0530

    Optimized enemy behaviour

--- Assets/scripts/EnemyAiTutorial.cs
+++ Assets/scripts/EnemyAiTutorial.cs
@@ -1,21 +1,18 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.UI;
 
 public class EnemyAiTutorial : MonoBehaviour
 {
-    private float lerpSpeed = 5f; // Adjust for smoothness
-
     public NavMeshAgent agent;
+
     public Transform player;
-    public LayerMask whatIsGround, whatIsPlayer, whatIsObstacle;
-    public float health, maxHealth = 100;
-    public Transform BulletSpawn;
-    public Slider healthBar; // Reference to the health bar slider
 
-    Animator animator;
+    public LayerMask whatIsGround, whatIsPlayer;
 
-    //Patrolling
+    public float health;
+
+    //Patroling
     public Vector3 walkPoint;
     bool walkPointSet;
     public float walkPointRange;
@@ -23,90 +20,29 @@ public class EnemyAiTutorial : MonoBehaviour
     //Attacking
     public float timeBetweenAttacks;
     bool alreadyAttacked;
-    public GameObject bullet;
+    public GameObject projectile;
 
     //States
     public float sightRange, attackRange;
-    public bool playerInSightRange, playerInAttackRange, playerVisible;
+    public bool playerInSightRange, playerInAttackRange;
 
     private void Awake()
     {
-        player = GameObject.Find("OVRCameraRig").transform;
+        player = GameObject.Find("PlayerObj").transform;
         agent = GetComponent<NavMeshAgent>();
-        animator = GetComponent<Animator>();
-
-        health = maxHealth; // Initialize health
-        if (healthBar) healthBar.value = health / maxHealth;
     }
 
     private void Update()
     {
-        animator.SetFloat("Speed", agent.velocity.magnitude);
-
-        // Update health bar position
-        //if (healthBar)
-        //{
-        //    healthBar.transform.position = transform.position + Vector3.up * 2f; // Adjust height
-        //    healthBar.transform.LookAt(player); // Face player
-        //}
-
         //Check for sight and attack range
         playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
         playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
 
-        Vector3 direction = transform.forward;
-
-        Debug.DrawRay(BulletSpawn.position, direction * attackRange, Color.red, 1.0f);
-
-        RaycastHit hit;
-
-        if (Physics.Raycast(BulletSpawn.position, direction, out hit, attackRange))
-        {
-            if (hit.collider.gameObject.name == "OVRCameraRig") // Ensures enemy sees the player
-            {
-                playerVisible = true;
-                Debug.Log(hit.collider.gameObject.name);
-            }
-        }
-        else
-        {
-            playerVisible= false;
-        }
-
-        Debug.Log(playerVisible);
-
         if (!playerInSightRange && !playerInAttackRange) Patroling();
         if (playerInSightRange && !playerInAttackRange) ChasePlayer();
-        if (playerInAttackRange && playerInSightRange && !playerVisible) ChasePlayer();
-        if (playerInAttackRange && playerInSightRange && playerVisible) AttackPlayer();
-
-        /*bool hasClearSight = false;
-        if (playerInSightRange)
-        {
-            RaycastHit hit;
-            Vector3 directionToPlayer = (player.position - transform.position).normalized;
-
-            if (Physics.Raycast(transform.position, directionToPlayer, out hit, sightRange, ~whatIsGround))
-            {
-                if (hit.transform == player) // Ensures enemy only sees player if no obstacle is in between
-                {
-                    hasClearSight = true;
-                }
-            }
-        }
-
-        if (!hasClearSight)
-        {
-            Patroling();
-            return;
-        }
-
-        if (hasClearSight && !playerInAttackRange)
-            ChasePlayer();
-        else if (hasClearSight && playerInAttackRange)
-            AttackPlayer();*/
+        if (playerInAttackRange && playerInSightRange) AttackPlayer();
     }
-    
+
     private void Patroling()
     {
         if (!walkPointSet) SearchWalkPoint();
@@ -139,28 +75,21 @@ public class EnemyAiTutorial : MonoBehaviour
 
     private void AttackPlayer()
     {
-        PlayerController controller = player.GetComponent<PlayerController>();
-        //controller.TakeDamage(10);
         //Make sure enemy doesn't move
+        agent.SetDestination(transform.position);
 
-        Vector3 direction = (player.position - BulletSpawn.position).normalized;
-        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
-        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+        transform.LookAt(player);
 
         if (!alreadyAttacked)
         {
             ///Attack code here
-            GameObject spawnedBullet = Instantiate(bullet, BulletSpawn.position, Quaternion.identity);
-            Rigidbody rb = spawnedBullet.GetComponent<Rigidbody>();
-
-            rb.linearVelocity = direction * 32f;
+            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+            rb.AddForce(transform.up * 8f, ForceMode.Impulse);
             ///End of attack code
-            
-            agent.SetDestination(transform.position);
 
             alreadyAttacked = true;
             Invoke(nameof(ResetAttack), timeBetweenAttacks);
-            Destroy(spawnedBullet, 2f);
         }
     }
     private void ResetAttack()
@@ -171,18 +100,12 @@ public class EnemyAiTutorial : MonoBehaviour
     public void TakeDamage(int damage)
     {
         health -= damage;
-        if (healthBar)
-        {
-            healthBar.value = Mathf.Lerp(healthBar.value, health / maxHealth, Time.deltaTime * lerpSpeed);
-        }
 
         if (health <= 0) Invoke(nameof(DestroyEnemy), 0.5f);
     }
-
     private void DestroyEnemy()
     {
         Destroy(gameObject);
-        GameStatistics.Instance.Kills += 1;
     }
 
     private void OnDrawGizmosSelected()
